e3cac7580f3822b8b7d5fa5769cda72dc1ca9b79
kalai <skalaiarasy@gmail.com>
2021-05-26 17:59:30 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoLab';
+  todo:Todo[] = [
+    {
+      task:"fold clothes",
+      completed:false,
+    },
+    {
+      task:"put clothes in dresser",
+      completed: false,
+    },
+    {
+      task:"relax",
+      completed: true,
+    }
+  ];
+  addTask(form:NgForm):void{
+    let newTask:Todo = {
+      task:form.form.value.task,
+      completed:false
+    };
+    this.todo.push(newTask);
+  }
+  completeTask(form:NgForm):void{
+    this.todo[form.form.value.completed].completed = true;
+  }
+
 }
