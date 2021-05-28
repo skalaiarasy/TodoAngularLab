1a3af39faff64534607340c5f58ffa2e50a92086
kalai <skalaiarasy@gmail.com>
2021-05-27 21:34:29 -0400

    Component fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,4 @@ import { Todo } from './todo';
 })
 export class AppComponent {
   title = 'TodoLab';
-  todo:Todo[] = [
-    {
-      task:"fold clothes",
-      completed:false,
-    },
-    {
-      task:"put clothes in dresser",
-      completed: false,
-    },
-    {
-      task:"relax",
-      completed: true,
-    }
-  ];
-  addTask(form:NgForm):void{
-    let newTask:Todo = {
-      task:form.form.value.task,
-      completed:false
-    };
-    this.todo.push(newTask);
-  }
-  completeTask(form:NgForm):void{
-    this.todo[form.form.value.completed].completed = true;
-  }
-  //wishMessage()
-  // removeTask(form:NgForm):void{
-  //   let result:
-  // }
-  removeTask(i:any)
-  {
-    this.todo.splice(i,1);
-  }
-
-
-
-
 }
--- src/app/routine/routine.component.ts
+++ src/app/routine/routine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-routine',
@@ -11,5 +13,38 @@ export class RoutineComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  todo:Todo[] = [
+    {
+      task:"fold clothes",
+      completed:false,
+    },
+    {
+      task:"put clothes in dresser",
+      completed: false,
+    },
+    {
+      task:"relax",
+      completed: true,
+    }
+  ];
+  addTask(form:NgForm):void{
+    let newTask:Todo = {
+      task:form.form.value.task,
+      completed:false
+    };
+    this.todo.push(newTask);
+  }
+  completeTask(form:NgForm):void{
+    this.todo[form.form.value.completed].completed = true;
+  }
+  
+  completeMessage(form:NgForm):void{
+
+  }
+  removeTask(i:any)
+  {
+    this.todo.splice(i,1);
+  }
+
 
 }
